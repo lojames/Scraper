143eb07b4d4484f044e631150b070394ed18d7c0
James Lo <lo.jamess@gmail.com>
2019-05-08 23:58:31 -0400

    Outline implementation plan in comments.

--- scraper.rb
+++ scraper.rb
@@ -7,3 +7,41 @@ end
 
 url = "https://yawp-app.herokuapp.com"
 puts url_source_to_string(url)
+
+###Business
+#name
+#neighborhood (opt)
+#street_address
+#city
+#state
+#zip
+#phone (opt)
+#website (opt)
+#price (opt)
+#latitude
+#longitude
+
+###Business Hours
+#day
+#hours
+#business_id
+
+###Business Categories
+#Scrape all business categories into an array
+#FOR SEEDING: search categories by "ref" to get category id
+
+###Business Properties
+#Scrape all business properties into an array
+#FOR SEEDING: search categories by "key" and "value" to get property id...if ID doesn't exist, create new business property
+
+###Reviews
+#body
+#business_id
+#score
+
+###Images
+#image_url (generate after scraping)
+#comment
+#user_id
+#business_id
+#review_id
