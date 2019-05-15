c8237fbc44fdf866cabd563ccf70200619f6b7ed
James Lo <lo.jamess@gmail.com>
2019-05-15 10:01:31 -0500

    Load first names and last from YAML file.

--- names.rb
+++ names.rb
@@ -1,5 +1,11 @@
+require 'yaml'
+
 characters = 'abcdefghijklmnopqrstuvwxyz'
 domains = ['@gmail.com', '@outlook.com', '@yahoo.com']
 
-first_names = []
-last_names = []
+names = YAML.load(File.read("names.yml"))
+first_names = names[:first_names]
+last_names = names[:last_names]
+
+puts first_names.to_s
+puts last_names.to_s
