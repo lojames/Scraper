6d14392bfa636e66aa3e5b0f9ea48e0dedcd81ed
James Lo <lo.jamess@gmail.com>
2019-05-17 18:12:22 -0500

    Outline methods in UserGenerator class.

--- user_generator.rb
+++ user_generator.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 class UserGenerator
   attr_reader :first_names, :last_names
-  @characters = 'abcdefghijklmnopqrstuvwxyz'
+  @characters = 'abcdefghijklmnopqrstuvwxyz'.split("")
   @domains = ['@gmail.com', '@outlook.com', '@yahoo.com']
 
   def initialize
@@ -11,6 +11,31 @@ class UserGenerator
     @last_names = names[:last_names]
   end
 
+  def random_name
+
+  end
+
+  def random_male_name
+
+  end
+
+  def random_female_name
+
+  end
+
+  def random_email
+
+  end
+
+  def names(num)
+
+  end
+
+  def emails(num)
+    
+  end
+
 end
 
-puts UserGenerator.new.last_names
+generator = UserGenerator.new
+puts generator.last_names
