608b500130b9e0ff2a49689a9c0196ded11d25aa
James Lo <lo.jamess@gmail.com>
2019-05-22 17:05:51 -0400

    Implement random_emails and emails methods.

--- user_generator.rb
+++ user_generator.rb
@@ -1,41 +1,54 @@
 require 'yaml'
 
 class UserGenerator
-  attr_reader :first_names, :last_names
-  @characters = 'abcdefghijklmnopqrstuvwxyz'.split("")
-  @domains = ['@gmail.com', '@outlook.com', '@yahoo.com']
 
   def initialize
+    @characters = 'abcdefghijklmnopqrstuvwxyz'.split("")
+    @domains = ['@gmail.com', '@outlook.com', '@yahoo.com']
+
     names = YAML.load(File.read("names.yml"))
     @first_names = names[:first_names]
     @last_names = names[:last_names]
   end
 
   def random_name
-    first_names.sample + " " + last_names.sample
+    @first_names.sample + " " + @last_names.sample
   end
 
   def random_male_name
-    first_names[0...30].sample + " " + last_names.sample
+    @first_names[0...30].sample + " " + @last_names.sample
   end
 
   def random_female_name
-    first_names[30...60].sample + " " + last_names.sample
+    @first_names[30...60].sample + " " + @last_names.sample
   end
 
   def random_email
-
+    @characters.sample + @domains.sample
   end
 
   def names(num=60)
-
+    count = 0
   end
 
   def emails(num=78)
-
+    count = 0
+    emails = []
+    until count == num
+      @characters.each do |c|
+        @domains.each do |d|
+          emails << (c+d)
+          count+=1
+          return emails if count == num
+        end
+      end
+    end
+    emails
   end
 
 end
 
 generator = UserGenerator.new
-puts generator.first_names[30...60]
+puts generator.emails.length
+puts generator.emails.first
+puts generator.emails.last
